9b1139dcd50462ae49bfa0e232aef626f5d6c66c
smulionOong <1224police@likelion.org>
2019-12-07 22:09:05 +0900

    done

--- delivery/storage.c
+++ delivery/storage.c
@@ -21,19 +21,19 @@
   char *contents : package context (message string)
 */
 typedef struct {
-    int building;
-    int room;
-    int cnt;
-    char passwd[PASSWD_LEN+1];
-    
-    char *context;
+   int building;
+   int room;
+   int cnt;
+   char passwd[PASSWD_LEN+1];
+   
+   char *context;
 } storage_t;
 
 
-static storage_t** deliverySystem;             //deliverySystem
-static int storedCnt = 0;                    //number of cells occupied
-static int systemSize[2] = {0, 0};          //row/column of the delivery system
-static char masterPassword[PASSWD_LEN+1];    //master password
+static storage_t** deliverySystem;          //deliverySystem
+static int storedCnt = 0;               //number of cells occupied
+static int systemSize[2] = {0, 0};        //row/column of the delivery system
+static char masterPassword[PASSWD_LEN+1];   //master password
 
 
 
@@ -43,15 +43,15 @@ static char masterPassword[PASSWD_LEN+1];    //master password
 //print the inside context of a specific cell
 //int x, int y : cell to print the context
 static void printStorageInside(int x, int y) {
-    printf("\n------------------------------------------------------------------------\n");
-    printf("------------------------------------------------------------------------\n");
-    if (deliverySystem[x][y].cnt > 0)
-        printf("<<<<<<<<<<<<<<<<<<<<<<<< : %s >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", deliverySystem[x][y].context);
-    else
-        printf("<<<<<<<<<<<<<<<<<<<<<<<< empty >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-        
-    printf("------------------------------------------------------------------------\n");
-    printf("------------------------------------------------------------------------\n\n");
+   printf("\n------------------------------------------------------------------------\n");
+   printf("------------------------------------------------------------------------\n");
+   if (deliverySystem[x][y].cnt > 0)
+      printf("<<<<<<<<<<<<<<<<<<<<<<<< : %s >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n", deliverySystem[x][y].context);
+   else
+      printf("<<<<<<<<<<<<<<<<<<<<<<<< empty >>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+      
+   printf("------------------------------------------------------------------------\n");
+   printf("------------------------------------------------------------------------\n\n");
 }
 
 //initialize the storage
@@ -59,14 +59,38 @@ static void printStorageInside(int x, int y) {
 //and allocate memory to the context pointer
 //int x, int y : cell coordinate to be initialized
 static void initStorage(int x, int y) {
-    
+
+       deliverySystem[x][y].building = 0;
+       deliverySystem[x][y].room = 0;
+       strcpy(deliverySystem[x][y].passwd, "pass");
+         deliverySystem[x][y].cnt = 0;
+         deliverySystem[x][y].context = (char*)malloc(sizeof(char)*20);
+         strcpy(deliverySystem[x][y].context, "context");
+   
 }
 
 //get password input and check if it is correct for the cell (x,y)
 //int x, int y : cell for password check
 //return : 0 - password is matching, -1 - password is not matching
 static int inputPasswd(int x, int y) {
-    
+   
+   int index; // index is for compare the password. if index = 0 then match, if index = -1 then wrong
+   char pass[PASSWD_LEN+1];
+   
+   printf("input password for (%d, %d) storage :", x, y);
+   scanf("%s", &pass);
+   
+   index = strcmp(pass, deliverySystem[x][y].passwd);
+   
+   if(index == 0)
+   {
+      return 0;
+   }
+   else
+   {
+      return -1;
+   }
+   
 }
 
 
@@ -89,16 +113,16 @@ int str_backupSystem(char* filepath) {
       return -1;
    }
    
-   fprintf(fp, "%d %d", systemSize[0], systemSize[1]);
-   fprintf(fp, "%s", masterPassword);
+   fprintf(fp, "%d %d\n", systemSize[0], systemSize[1]);
+   fprintf(fp, "%s\n", masterPassword);
    
    for(i=0;i<systemSize[0]; i++)
    {
       for(j=0;j<systemSize[1]; j++)
       {
-         if(deliverySystem[i][j].cnt == 1)
+         if(deliverySystem[i][j].cnt > 0)
          {
-            fprintf(fp, "%d %d %s ",deliverySystem[i][j].building, deliverySystem[i][j].room, deliverySystem[i][j].passwd);
+            fprintf(fp, "%d %d %d %d %s ",i,j,deliverySystem[i][j].building, deliverySystem[i][j].room, deliverySystem[i][j].passwd);
 
             fprintf(fp, "%s\n",deliverySystem[i][j].context);
          }
@@ -118,129 +142,124 @@ int str_backupSystem(char* filepath) {
 //char* filepath : filepath and name to read config parameters (row, column, master password, past contexts of the delivery system
 //return : 0 - successfully created, -1 - failed to create the system
 int str_createSystem(char* filepath) {
-    int i;
-    //int j;
-        int x,y;
-        FILE *fp;
-        
-        //this makes deliverySystem dimension array (like delivery box)
-        fp   = fopen( "sample.txt", "r");
-        fscanf( fp, "%d %d", &systemSize[0], &systemSize[1]);
-
-        deliverySystem = (storage_t**)malloc(sizeof(storage_t*)*systemSize[1]);
-        
-        /*
-         int **A;
-         A = (int**)
-         
-         
-         */
-        for(i=0;i<systemSize[0];i++)
-        {
-            deliverySystem[i] = (storage_t*)malloc(sizeof(storage_t)*systemSize[1]);
-        }
-        
-        //this sets masterPassword
-        fscanf( fp, "%s", masterPassword);
-        
-        while( !feof( fp ) ) // if file is available
-        {
-            //this is about delivery information
-            //ex: 0 0 3 103 1234 noPassword
-            //from building number to passwd
-            fscanf( fp, "%d %d", &x, &y);//row, column
-            deliverySystem[x][y].cnt++;
-            fscanf( fp, "%d %d %s", &deliverySystem[x][y].building, &deliverySystem[x][y].room, deliverySystem[x][y].passwd);//building, room, passwd
-            //context
-            deliverySystem[x][y].context = (char*)malloc(sizeof(char)*20);
-            fscanf(fp,"%s",deliverySystem[x][y].context);
-        }
-        
-    //    for(i=0;i<systemSize[0];i++)
-    //    {
-    //        for(j=0;j<systemSize[1];j++)
-    //        {
-    //            printf("%d\n",deliverySystem[i][j].building);
-    //            printf("%d\n",deliverySystem[i][j].room);
-    //            printf("%s\n",deliverySystem[i][j].passwd);
-    //            printf("%s\n",deliverySystem[i][j].context);
-    //            //free(deliverySystem[i][j].context);
-    //        }
-    //    }
-        
-            
-    //    for(i=0;i<systemSize[0];i++)
-    //    {
-    //        for(j=0;j<systemSize[1];j++)
-    //        {
-    //            free(deliverySystem[i][j].context);
-    //        }
-    //    }
-        
-        fclose(fp);
+   int i;
+    int j;
+    int x,y;
+    FILE *fp;
     
-    return 0;
+    //this makes deliverySystem dimension array (like delivery box)
+    fp   = fopen(filepath, "r");
+    fscanf( fp, "%d %d", &systemSize[0], &systemSize[1]); // accept row, column
+    
+    
+   deliverySystem = (storage_t**)malloc(sizeof(storage_t*)*systemSize[0]); // create row
+    
+    for(i=0;i<systemSize[0];i++)
+    {
+        deliverySystem[i] = (storage_t*)malloc(sizeof(storage_t)*systemSize[1]); // create column
+    }
+    
+    for(i=0;i<systemSize[0];i++)
+    {
+       for(j=0;j<systemSize[1];j++)
+       {
+           initStorage(i, j); // initialize the delivery box before creating struct srorage_t
+      }
+   }
+   
+    
+    //this sets masterPassword
+    fscanf(fp, "%s", masterPassword);
+    
+    while(!feof(fp)) // if file is available
+    {
+        //this is about delivery information
+        //ex: 0 0 3 103 1234 noPassword
+        //from building number to passwd
+        
+        fscanf( fp, "%d %d", &x, &y);//row, column
+
+        fscanf( fp, "%d %d %s", &deliverySystem[x][y].building, &deliverySystem[x][y].room, deliverySystem[x][y].passwd);//building, room, passwd
+
+      deliverySystem[x][y].cnt = 1; // it means there is a package
+        
+        fscanf(fp,"%s",deliverySystem[x][y].context);
+
+      storedCnt++;
+        
+    }
+ 
+    //str_freeSystem(); this makes wrong result;;
+    
+    fclose(fp);
+
+
+   return 0;
 }
 
 //free the memory of the deliverySystem
 void str_freeSystem(void) {
-    int i;
-    
-    for(i=0;i<systemSize[0];i++)
+   int i;
+   
+   for(i=0;i<systemSize[0];i++)
     {
         free(deliverySystem[i]);
     }
     free(deliverySystem);
+   
 }
 
 
 
 //print the current state of the whole delivery system (which cells are occupied and the destination of the each occupied cells)
 void str_printStorageStatus(void) {
-    int i, j;
-    printf("----------------------------- Delivery Storage System Status (%i occupied out of %i )-----------------------------\n\n", storedCnt, systemSize[0]*systemSize[1]);
-    
-    printf("\t");
-    for (j=0;j<systemSize[1];j++)
-    {
-        printf(" %i\t\t",j); // show row
-    }
-    printf("\n-----------------------------------------------------------------------------------------------------------------\n");
-    
-    for (i=0;i<systemSize[0];i++)
-    {
-        printf("%i|\t",i); // show column
-        for (j=0;j<systemSize[1];j++)
-        {
-            if (deliverySystem[i][j].cnt > 0) // if resident has some packagem then show
-            {
-                printf("%i,%i\t|\t", deliverySystem[i][j].building, deliverySystem[i][j].room);
-            }
-            else // cnt == 0 means no package
-            {
-                printf(" -  \t|\t");
-            }
-        }
-        printf("\n");
-    }
-    printf("--------------------------------------- Delivery Storage System Status --------------------------------------------\n\n");
+   int i, j;
+   printf("----------------------------- Delivery Storage System Status (%i occupied out of %i )-----------------------------\n\n", storedCnt, systemSize[0]*systemSize[1]);
+   
+   
+   
+   printf("\t");
+   for (j=0;j<systemSize[1];j++)
+   {
+      printf(" %i\t\t",j); // show row
+   }
+   printf("\n-----------------------------------------------------------------------------------------------------------------\n");
+   
+   for (i=0;i<systemSize[0];i++)
+   {
+      printf("%i|\t",i); // show column
+      for (j=0;j<systemSize[1];j++)
+      {
+         if (deliverySystem[i][j].cnt != 0) // if resident has some packagem then show
+         {
+            printf("%i,%i\t|\t", deliverySystem[i][j].building, deliverySystem[i][j].room);
+            
+         }
+         else // cnt == 0 means no package
+         {
+            printf(" -  \t|\t");
+         }
+      }
+      printf("\n");
+   }
+   printf("--------------------------------------- Delivery Storage System Status --------------------------------------------\n\n");
 }
 
 
 
 //check if the input cell (x,y) is valid and whether it is occupied or not
 int str_checkStorage(int x, int y) {
-    if (x < 0 || x >= systemSize[0])
-    {
-        return -1;
-    }
-    
-    if (y < 0 || y >= systemSize[1])
-    {
-        return -1;
-    }
-    
-    return deliverySystem[x][y].cnt;
+   if (x < 0 || x >= systemSize[0])
+   {
+      return -1;
+   }
+   
+   if (y < 0 || y >= systemSize[1])
+   {
+      return -1;
+   }
+   
+   return deliverySystem[x][y].cnt;
 }
 
 
@@ -252,27 +271,29 @@ int str_checkStorage(int x, int y) {
 //char passwd[] : password string (4 characters)
 //return : 0 - successfully put the package, -1 - failed to put
 int str_pushToStorage(int x, int y, int nBuilding, int nRoom, char msg[MAX_MSG_SIZE+1], char passwd[PASSWD_LEN+1]) {
-    int i;
-    
-    if(deliverySystem[x][y].cnt == 0)
-    {
-        deliverySystem[x][y].building = nBuilding;
-        deliverySystem[x][y].room = nRoom;
-        deliverySystem[x][y].context = msg;
-        for (i=0;i<sizeof(passwd[PASSWD_LEN+1]);i++)
-        {
-           deliverySystem[x][y].passwd[i] = passwd[i];
-        }
-        
-        
-        deliverySystem[x][y].cnt++;
-    }
-    else
-    {
-        return -1;
-    }
-
-    return 0;
+   
+   int i;
+   
+   
+   
+   if(deliverySystem[x][y].cnt == 0)
+   {
+      deliverySystem[x][y].building = nBuilding;
+      deliverySystem[x][y].room = nRoom;
+      deliverySystem[x][y].context = msg;
+      for (i=0;i<sizeof(passwd[PASSWD_LEN+1]);i++)
+      {
+         deliverySystem[x][y].passwd[i] = passwd[i];
+      }
+      
+      deliverySystem[x][y].cnt=1;
+      storedCnt++;
+   }
+   else
+   {
+      return -1;
+   }
+   return 0;
 }
 
 
@@ -282,8 +303,25 @@ int str_pushToStorage(int x, int y, int nBuilding, int nRoom, char msg[MAX_MSG_S
 //int x, int y : coordinate of the cell to extract
 //return : 0 - successfully extracted, -1 = failed to extract
 int str_extractStorage(int x, int y) {
-    
-    return 0;
+   
+   char inputPasswd[10];
+   
+   printf("input password for (%d, %d) storage :", x, y);
+   scanf("%s", inputPasswd);
+
+   
+//   inputPasswd(x, y)
+   if(strcmp(deliverySystem[x][y].passwd, inputPasswd))
+   {
+      printf("extracting the storage (%d, %d)...\n", x, y);
+      printStorageInside(x, y);
+      storedCnt--;
+      initStorage(x, y);
+      return 0;
+   }
+   else
+      return -1;
+
 }
 
 //find my package from the storage
@@ -293,29 +331,26 @@ int str_extractStorage(int x, int y) {
 int str_findStorage(int nBuilding, int nRoom) {
    
    int i, j;
+   int check = 0;
    
    for (i=0;i<systemSize[0];i++)
    {
       for (j=0;j<systemSize[1];j++)
       {
-         if (deliverySystem[i][j].cnt > 0) // if resident has some packagem then show
+         if(nBuilding == deliverySystem[i][j].building && nRoom == deliverySystem[i][j].room)
          {
-            if(nBuilding == deliverySystem[i][j].building && nRoom == deliverySystem[i][j].room)
-            {
-               printf("-----------> Found a package in (%d, %d)\n", i, j);
-               return 1;
-            }
-            else // when there is no resisdent information then the resident failed to find her package
-            {
-               return 0;
-            }
-   
-         }
-         else // cnt == 0 means no package
-         {
-            return 0; // 0 = failed to find my package
+            printf("-----------> Found a package in (%d, %d)\n", i, j);
+            check++;
          }
       }
    }
+   
+   
+   
+   if(check == 0)
+   {
+      return 0;
+   }
+   
    return 1;
 }
